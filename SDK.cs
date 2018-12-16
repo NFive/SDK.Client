99100823f19fec67159a590ed370cbcb50189954
Joe Biellik <joebiellik@gmail.com>
2018-12-16 19:01:57 +0000

    Add SDK version constant

---
+++ SDK.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+
+namespace NFive.SDK.Client
+{
+	/// <summary>
+	/// Client SDK constants.
+	/// </summary>
+	[PublicAPI]
+	public static class SDK
+	{
+		/// <summary>
+		/// The client SDK version number.
+		/// </summary>
+		public const int Version = 1;
+	}
+}
