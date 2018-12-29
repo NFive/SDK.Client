e95ba5dce1193c93493b83ff6443538421e36abf
Igi <brad@igi.codes>
2018-12-29 01:10:11 +0000

    Updated SDK.Core ref and added Commands.ICommandManager

---
+++ Commands/ICommandManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NFive.SDK.Client.Commands
+{
+	public interface ICommandManager
+	{
+		void Register<T>(string command, Action<T> callback);
+
+		void Register(string command, Action callback);
+	}
+}
