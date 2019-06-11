1a598a21ca43ea5ef1ad91d6304c2468ddfa1eee
Joe Biellik <joebiellik@gmail.com>
2019-06-11 16:01:47 +0100

    Refactor RPC interfaces for more abstraction

--- Rpc/IRpcEvent.cs
+++ Rpc/IRpcEvent.cs
@@ -2,11 +2,24 @@ using JetBrains.Annotations;
 
 namespace NFive.SDK.Client.Rpc
 {
+	/// <summary>
+	/// Represents a received RPC event.
+	/// </summary>
 	[PublicAPI]
 	public interface IRpcEvent
 	{
+		/// <summary>
+		/// Gets the event name.
+		/// </summary>
+		/// <value>
+		/// The event name.
+		/// </value>
 		string Event { get; }
 
+		/// <summary>
+		/// Replies to the event with the specified payloads.
+		/// </summary>
+		/// <param name="payloads">The payloads to reply to the event with.</param>
 		void Reply(params object[] payloads);
 	}
 }
--- Rpc/IRpcHandler.cs
+++ Rpc/IRpcHandler.cs
@@ -5,6 +5,11 @@ namespace NFive.SDK.Client.Rpc
 	[PublicAPI]
 	public interface IRpcHandler
 	{
+		/// <summary>
+		/// Events the specified event.
+		/// </summary>
+		/// <param name="event">The event.</param>
+		/// <returns></returns>
 		IRpc Event(string @event);
 	}
 }
