9bde5bc021429dab51fc60185963a6ace2855c10
Joe Biellik <joebiellik@gmail.com>
2019-10-29 15:33:42 +0000

    Update and improve hotkey and input handling

--- Input/Hotkey.cs
+++ Input/Hotkey.cs
@@ -1,29 +1,57 @@
-using System;
-using CitizenFX.Core;
+using System.Linq;
 using CitizenFX.Core.Native;
 using JetBrains.Annotations;
+using NFive.SDK.Core.Input;
 
 namespace NFive.SDK.Client.Input
 {
 	[PublicAPI]
 	public class Hotkey
 	{
-		public Control Control { get; }
+		public InputControl Control { get; }
 
-		public KeyCode KeyCode { get; }
+		public string ControlDisplayName => Input.ControlDisplayNames.ElementAtOrDefault((int)this.Control) ?? "Unknown";
+
+		public string ControlNativeName => Input.ControlNativeNames.ElementAtOrDefault((int)this.Control) ?? "Unknown";
 
 		public string ScaleformName { get; }
 
-		public string DisplayName { get; }
+		public KeyCode UserKeyboardKey { get; }
 
-		public Hotkey(Control control)
+		public string UserKeyboardKeyDisplayName => Input.KeyCodeDisplayNames.TryGetValue(this.UserKeyboardKey, out var value) ? value : "Unknown";
+
+		public KeyCode DefaultKeyboardKey => Input.ControlKeyCodeMapping.ElementAtOrDefault((int)this.Control);
+
+		public string DefaultKeyboardKeyDisplayName => Input.KeyCodeDisplayNames.TryGetValue(this.DefaultKeyboardKey, out var value) ? value : "Unknown";
+
+		public ButtonCode ControllerButton => Input.ControlButtonCodeMapping.ElementAtOrDefault((int)this.Control);
+
+		public string ControllerButtonDisplayName => Input.ButtonCodeDisplayNames.ElementAtOrDefault((int)this.ControllerButton);
+
+		public Hotkey(InputControl control)
 		{
 			this.Control = control;
-			this.ScaleformName = API.GetControlInstructionalButton(0, (int)this.Control, 0);
-			this.KeyCode = Input.KeyMappings.ContainsKey(this.ScaleformName) ? Input.KeyMappings[this.ScaleformName] : KeyCode.None; // TODO: KeyCode.Unknown ?
-			this.DisplayName = this.KeyCode == KeyCode.None ? this.ScaleformName : Enum.GetName(typeof(KeyCode), this.KeyCode);
+			this.ScaleformName = API.GetControlInstructionalButton(0, (int)control, 0); // InputGroup doesn't seem to do anything
+			this.UserKeyboardKey = Input.ScaleformKeyCodeMappings.TryGetValue(this.ScaleformName, out var value) ? value : KeyCode.None;
+			if (this.UserKeyboardKey == KeyCode.Unknown) this.UserKeyboardKey = this.DefaultKeyboardKey; // TODO: Safe?
 		}
 
-		public override string ToString() => this.DisplayName;
+		public bool IsPressed()
+		{
+			return Input.IsControlPressed(this.Control);
+		}
+
+		public bool IsJustPressed()
+		{
+
+			return Input.IsControlJustPressed(this.Control);
+		}
+
+		public bool IsJustReleased()
+		{
+			return Input.IsControlJustReleased(this.Control);
+		}
+
+		public override string ToString() => $"{this.UserKeyboardKeyDisplayName}/{this.ControllerButtonDisplayName} ({this.ControlDisplayName})";
 	}
 }
